606a56d6cde1bd54e66383cdffb482bfd3179e77
Ogawa Daisuke <99077817+ogawa-daisuke514@users.noreply.github.com>
2022-02-14 15:00:20 +0000

    [Stop] 3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,44 @@
 class BooksController < ApplicationController
-  def new
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice: "A book was successfully created."
+    else
+      render :index
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
+
+  def update
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    @message =
+    @books = Book.all
+    @book = Book.new
+    # render :index
+    redirect_to books_path, notice: "A book was successfully deleted."
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
